7decad4d41c63c43d5d919d9a0eba5c467993e9f
Novikoviliya <juniorwebdevel@gmail.com>
2021-01-15 07:59:39 +0300

    Ready build

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,16 @@
 "use-strict";
-const snap = document.querySelector(".main-nav__toggle");
-const nav = document.querySelector(".main-nav__list");
-snap.onclick = function(evt) {
-  evt.preventDefault();
-  nav.classList.toggle('site-list');
-};
+var menuToggle = document.querySelector(".main-nav__toggle");
+var nav = document.querySelector(".main-nav__list");
+var main = document.querySelector(".main-nav");
+menuToggle.addEventListener("click", function() {
+  if (nav.classList.contains("site-list")) {
+    nav.classList.remove("site-list");
+  } else { nav.classList.add("site-list"); }
+  if (main.classList.contains("main-nav__menu--opened")) {
+    main.classList.remove("main-nav__menu--opened");
+    main.classList.add("main-nav__menu--closed");
+  } else {
+    main.classList.remove("main-nav__menu--closed");
+    main.classList.add("main-nav__menu--opened");
+  }
+});
