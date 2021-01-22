ce8bceb2771e4c55922a2599a3d8487b3a2d93b2
Novikoviliya <juniorwebdevel@gmail.com>
2021-01-22 15:17:34 +0300

    ..

--- source/js/script.js
+++ source/js/script.js
@@ -1,13 +1,6 @@
 "use-strict";
 var menuToggle = document.querySelector(".page-header__wrapper-toggle");
 var nav = document.querySelector(".main-nav__list");
-<<<<<<< HEAD
-nav.classList.add('site-list--shadow');
-menuToggle.classList.add('page-header__wrapper-toggle--show');
-=======
-nav.classList.add("site-list--shadow");
-menuToggle.classList.add("page-header__wrapper-toggle--show");
->>>>>>> 2485341b1bf61a3d23e98e8f909b4eb99667e6fe
 menuToggle.addEventListener("click", function() {
   if (nav.classList.contains("site-list--shadow")) {
     nav.classList.remove("site-list--shadow");
